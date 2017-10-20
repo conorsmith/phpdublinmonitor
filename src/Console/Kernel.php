a11d97714c4718276ede68a3827de33475e58163
Conor Smith <conorjbsmith@gmail.com>
2017-10-20 12:44:54 +0100

    Use Symfony console in our console kernel

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace ConorSmith\PhpDublinMonitor\Console;
 
+use Symfony\Component\Console\Application;
+
 class Kernel
 {
     public function handle(): void
     {
-        //
+        $delegateKernel = new Application;
+
+        $delegateKernel->run();
     }
 }
