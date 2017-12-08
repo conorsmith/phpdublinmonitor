8a4787c175cd3f0b57661cc0fc298de5d9b484b8
Conor Smith <conorjbsmith@gmail.com>
2017-12-08 10:49:30 +0000

    Create action for web

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
 declare(strict_types=1);
 
-echo "placeholder text";
+$application = require_once __DIR__ . "/../bootstrap.php";
+
+$action = $application->make(\ConorSmith\PhpDublinMonitor\Web\DisplayWebsiteStatusAction::class);
+
+$action();
---
+++ src/Web/DisplayWebsiteStatusAction.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace ConorSmith\PhpDublinMonitor\Web;
+
+class DisplayWebsiteStatusAction
+{
+    public function __invoke(): void
+    {
+        echo "placeholder text";
+    }
+}
